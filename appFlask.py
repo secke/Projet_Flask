cb8fd6a3241a035faf58d7a99eecdb1f456f623f
lamoundioss7 <lamoundioss7@gmail.com>
2022-04-18 23:32:41 +0000

    Réalisation de la page principale du projet Flask

--- appFlask.py
+++ appFlask.py
@@ -1,8 +1,19 @@
-from flask import Flask, url_for,render_template
+from flask import Flask, url_for,render_template, redirect
+# from flask_sqlalchemy import SQLAlchemy
+
+
 app=Flask(__name__)
+
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:test_123@localhost/flask_project'
+
 @app.route('/')
-def index():
-    return render_template('base.html')
+def principal():
+    return render_template('principal.html')
+
+@app.route('/adduser')
+def adduser():
+    return render_template('adduser.html')
 
 if __name__=='__main__':
     app.run(debug=True)
