bccee965856d08f2c0e374a90892725f3bf2b4b5
secke <bocarkeita2010@gmail.com>
2022-04-22 00:54:51 +0000

    création  et modification de posts

--- base.py
+++ base.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 
 
-engine=create_engine('postgresql://secke:keita2022@localhost:5432/baseflask')
+# engine=create_engine('postgresql://secke:keita2022@localhost:5432/baseflask')
 engine=create_engine('postgresql://groupe7:test_123@localhost:5432/flask_db')
 base_session=sessionmaker(bind=engine,autocommit=False,autoflush=False)
 session=base_session()
